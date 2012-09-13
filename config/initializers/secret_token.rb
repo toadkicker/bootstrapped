3a3aff866ced6cc94902ef4d04564b6989cc99bb
Todd Baur <todd@toadkicker.com>
2012-09-13 11:04:02 -0700

    example rails app for bootstrap

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bootstrapped::Application.config.secret_token = '7624ba8ec6e5131eff93e57ed18c356d22fe9086234bf600b3816b2d564b413e98bb06265988952b6912275204da12ac29cb94de6809c5933a5052139d9af3a3'
